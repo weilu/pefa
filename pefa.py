b9ae8438276d26706c9b3ad957b9655d0f3c645c
Wei Lu <luwei.here@gmail.com>
2022-11-02 15:17:47 -0400

    Extract tables using tabula based on reviewed stage 1 output

--- pefa.py
+++ pefa.py
@@ -8,6 +8,7 @@ import PyPDF2
 import re
 import glob
 import unicodedata
+# import camelot
 
 
 # report language and primary, secondary, and tertiary keywords to use for table detection
@@ -48,10 +49,13 @@ def download_pdf(link_to_content, language, country):
 def page_has_table(pdf_path, page):
     return len(tabula.read_pdf(pdf_path, pages=page)) > 0
 
+def normalize_as_filename(filename):
+    return unicodedata.normalize('NFKD', filename)
+
 def find_tables(language, only_pdf=None):
     keyword, secondary_keywords, tertiary_keywords = config[language]
     results = []
-    for report in sorted(glob.glob(f"data/pdfs/{unicodedata.normalize('NFKD', language)}_*.pdf")):
+    for report in sorted(glob.glob(f"data/pdfs/{normalize_as_filename(language)}_*.pdf")):
         if only_pdf and report != only_pdf:
             continue
         table_start_page = None
@@ -113,24 +117,40 @@ def find_tables(language, only_pdf=None):
     return results
 
 
-meta_df = pd.read_csv('data/pefa-assessments.csv', encoding='utf-8')
-meta_df_to_process = meta_df[(meta_df.Type == 'National') & (meta_df.Availability == 'Public') & (meta_df.Framework == '2016 Framework')]
+def detect_table_start():
+    meta_df = pd.read_csv('data/pefa-assessments.csv', encoding='utf-8')
+    meta_df_to_process = meta_df[(meta_df.Type == 'National') & (meta_df.Availability == 'Public') & (meta_df.Framework == '2016 Framework')]
 
-# takes a few minutes (<5min) to complete
-for index, row in meta_df_to_process.iterrows():
-    download_pdf(row['Link to Content'], row['Language'], row['Country'])
+    # takes a few minutes (<5min) to complete
+    for index, row in meta_df_to_process.iterrows():
+        download_pdf(row['Link to Content'], row['Language'], row['Country'])
 
-stage1_processed_pdfs = []
-for lang in meta_df_to_process.Language.unique():
-    stage1_processed_pdfs += find_tables(lang)
-stage1_df = pd.DataFrame(stage1_processed_pdfs)
-stage1_df = stage1_df.astype({'table_start_page': 'Int64'})
-stage1_df['Link to Content'] = 'https://www.pefa.org/node/' + stage1_df.code
-stage1_df['table_last_page'] = ''
-stage1_df['comment'] = ''
-columns_ordered = ['code', 'pdf', 'Link to Content', 'table_start_page', 'table_last_page']
-stage1_df = stage1_df.reindex(columns=columns_ordered)
-stage1_df.to_csv('data/stage1.csv', index=False)
+    stage1_processed_pdfs = []
+    for lang in meta_df_to_process.Language.unique():
+        stage1_processed_pdfs += find_tables(lang)
+    stage1_df = pd.DataFrame(stage1_processed_pdfs)
+    stage1_df = stage1_df.astype({'table_start_page': 'Int64'})
+    stage1_df['Link to Content'] = 'https://www.pefa.org/node/' + stage1_df.code
+    stage1_df['table_last_page'] = ''
+    stage1_df['comment'] = ''
+    columns_ordered = ['code', 'pdf', 'Link to Content', 'table_start_page', 'table_last_page']
+    stage1_df = stage1_df.reindex(columns=columns_ordered)
+    stage1_df.to_csv('data/stage1.csv', index=False)
 
-# df = tabula.read_pdf("data/pdfs/711.pdf", pages='164')[0]
-# df.to_csv('data/afk_2014.csv')
+# detect_table_start()
+stage1_df = pd.read_csv('data/stage1_reviewed.csv', encoding='utf-8')
+stage1_df = stage1_df.astype({'table_start_page': 'Int64', 'table_last_page': 'Int64'})
+for index, row in stage1_df.iterrows():
+    pages = None
+    if pd.notnull(row.table_start_page):
+        pages = f'{row.table_start_page-1}-{row.table_last_page-1}'
+    if not pages:
+        print(f'No table in pdf {row.pdf}, skipping')
+        continue
+    tables = tabula.read_pdf(row.pdf, pages=pages)
+    folder_name = f"data/csvs/{Path(row.pdf).stem}"
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+    for i, table in enumerate(tables):
+        table.to_csv(f'{folder_name}/{i}.csv')
+    # tables = camelot.read_pdf(row.pdf, pages=pages, backend="poppler")
+
